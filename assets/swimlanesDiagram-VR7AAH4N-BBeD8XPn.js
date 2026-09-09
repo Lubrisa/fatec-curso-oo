0281e8874465483cc9e7078f407ba4798af5bbc9
Luigi C França <luigi.cfranca@gmail.com>
2026-09-09 14:52:14 -0300

    Updates

---
+++ assets/swimlanesDiagram-VR7AAH4N-BBeD8XPn.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-HODETNUW-Ccwdvxwg.js";import{_ as a}from"./index-C7xdLRo1.js";import"./chunk-5VM5RSS4-C3On3PPY.js";import"./chunk-XXDRQBXY-Dmqwx5h8.js";import"./chunk-POPQ4Y6H-CdQWvfu1.js";import"./chunk-F27PBJKO-EAA1Ze03.js";import"./channel-BGj--fjC.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
